660a6bfcafc4552d721d52e55039582fdc273abb
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2026-07-28 09:35:59 +0200

    Add negative tests for user authentication and authorization

--- src/aqa_api/users_api.py
+++ src/aqa_api/users_api.py
@@ -1,21 +1,21 @@
 from src.aqa_api.api_client import send_request
 
 
-def get_users():
-    return send_request("get", "/users")
+def get_users(headers: dict = None):
+    return send_request("get", "/users", headers=headers)
 
 
-def get_user_by_id(user_id: int):
-    return send_request("get", f"/users/{user_id}")
+def get_user_by_id(user_id: int, headers: dict = None):
+    return send_request("get", f"/users/{user_id}", headers=headers)
 
 
-def create_user(user_data: dict):
-    return send_request("post", "/users", request_body=user_data)
+def create_user(user_data: dict, headers: dict = None):
+    return send_request("post", "/users", request_body=user_data, headers=headers)
 
 
-def delete_user(user_id: int):
-    return send_request("delete", f"/users/{user_id}")
+def delete_user(user_id: int, headers: dict = None):
+    return send_request("delete", f"/users/{user_id}", headers=headers)
 
 
-def update_user(user_id: int, update_data: dict):
-    return send_request("put", f"/users/{user_id}", request_body=update_data)
+def update_user(user_id: int, update_data: dict, headers: dict = None):
+    return send_request("put", f"/users/{user_id}", request_body=update_data, headers=headers)
